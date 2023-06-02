1fae10a1f00bb103aaecad16cbfea7bfdb3be5d6
Dogeon188 <dogcatdog188@gmail.com>
2023-06-03 02:22:57 +0800

    reformat code

--- src/config.hpp
+++ src/config.hpp
@@ -10,16 +10,15 @@
 #define timeout 10
 #define MAX_STEP 50
 
-
 /*Which character/words for pieces*/
 /* By default, the pieces are '♟', '♜', '♞', '♝', '♛', '♚' from unicode*/
-// #define PIECE_STR_LEN 1
-// const char piece_table[2][7][5] = {
-//   {" ", "♟", "♜", "♞", "♝", "♛", "♚"},
-//   {" ", "♙", "♖", "♘", "♗", "♕", "♔"},
-// };
-#define PIECE_STR_LEN 2
+#define PIECE_STR_LEN 1
 const char PIECE_TABLE[2][7][5] = {
-  {"  ", "wP", "wR", "wK", "wB", "wQ", "wK"},
-  {"  ", "bP", "bR", "bK", "bB", "bQ", "bK"},
+#if PIECE_STR_LEN == 1
+    { " ", "♟", "♜", "♞", "♝", "♛", "♚" },
+    { " ", "♙", "♖", "♘", "♗", "♕", "♔" },
+#elif PIECE_STR_LEN == 2
+    { "  ", "wP", "wR", "wK", "wB", "wQ", "wK" },
+    { "  ", "bP", "bR", "bK", "bB", "bQ", "bK" },
+#endif
 };
